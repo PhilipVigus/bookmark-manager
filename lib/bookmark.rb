e2050b183d991d4a6df665e1fd3ee00b17c69f5e
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-06 15:02:09 +0100

    Add styling to bookmarks page

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark
   def self.all
-    [ { name: 'Google', url: 'www.google.com' },
-      { name: 'BBC', url: 'www.bbc.co.uk' } ]
+    [{ name: 'Google', url: 'www.google.com' },
+     { name: 'BBC', url: 'www.bbc.co.uk' }]
   end
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -3,4 +3,24 @@ feature 'You can view bookmarks' do
     visit '/bookmarks'
     expect(page).to have_content 'Bookmarks'
   end
+
+  scenario 'the page shows the first bookmark name' do
+    visit '/bookmarks'
+    expect(page).to have_content 'Google'
+  end
+
+  scenario 'the page shows the second bookmark name' do
+    visit '/bookmarks'
+    expect(page).to have_content 'BBC'
+  end
+
+  scenario 'the page shows the first bookmark url' do
+    visit '/bookmarks'
+    expect(page).to have_content 'www.google.com'
+  end
+
+  scenario 'the page shows the second bookmark url' do
+    visit '/bookmarks'
+    expect(page).to have_content 'www.bbc.co.uk'
+  end
 end
